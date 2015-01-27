dfd42eb563b04360f8e17172aec41ad93e1c3eaa
Tiago Jerónimo Silva <tiago_j.s@hotmail.com>
2015-01-27 02:53:52 +0000

    o trail esta a go««

--- src/Player.js
+++ src/Player.js
@@ -6,6 +6,8 @@ var Player = function(id, x, y, key) {
 	this.x = x;
 	this.y = y;
 	this.key = key;
+	this.groupTrail = game.add.group();
+	this.killTrail = false;
 };
 
 Player.prototype = {
@@ -20,19 +22,30 @@ Player.prototype = {
 		this.player.anchor.setTo(.5,.5);
 
 		game.physics.enable(this.player, Phaser.Physics.ARCADE);
+
+		game.time.events.add(Phaser.Timer.SECOND * 3, function(){this.killTrail = true;}, this);
+
 	},
 
 	update: function() {
 		this.player.body.angularVelocity = this.direction*200;
 		game.physics.arcade.velocityFromAngle(this.player.angle, 300, this.player.body.velocity);
 
-		trail = new Trail(this.id);
-		trail.create(this.player.x, this.player.y, this.size);
+		this.groupTrail.create(this.player.x, this.player.y, 'trail');
+		
+		if(this.killTrail){
+			var baddie = this.groupTrail.getFirstAlive();
+		    if (baddie)
+		    {
+		        baddie.kill();
+		    }
+		}
 
 		//game.input.onDown.add(this.click, this);
 		game.input.keyboard.addKey(this.key).onDown.add(this.click, this);
 
 	},
+
 
 	click: function() {
 		this.direction *= -1;
