43acf0ab1cca60ca374484a7a562cea7f5b53d33
Tiago Jerónimo Silva <tiago_j.s@hotmail.com>
2015-01-27 00:50:37 +0000

    sor

--- src/GameMananger.js
+++ src/GameMananger.js
@@ -1,47 +1,21 @@
 var GameMananger = function(g) {
 
 	game = g;
-	player = null;
-	textWriter = null;
-	objectsConstructor  = null;
-	this.playerCanMove = false;
-	levelTutorial = null;
 };
 
 GameMananger.prototype = {
 
 	preload: function() {
-    	levelTutorial = new LevelTutorial(game);
-		levelTutorial.preload();
-
-		player = new Player(game);
-		player.preload();
-
-		objectsConstructor = new ObjectsConstructor(game);
-		objectsConstructor.preload(); 
-
-		textWriter = new TextWriter(game);
-
-		this.playerCanMove = false;
 
 	},
 
 	create: function() {
-		levelTutorial.create();
-		player.create();
-
-		levelTutorial.textSequence();
 	},
 
 	update: function() {
-		if(this.playerCanMove){
-			player.update();
-			
-    	}
+
 	},
 
 	render: function(){
-		/*player.render();
-		objectsConstructor.render();*/
 	}
 };
