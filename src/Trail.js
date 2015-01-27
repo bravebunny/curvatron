7a5e5fd4a42ee1d6f8805bbc6cb7d46291d31970
Raicuparta <raicuparta@gmail.com>
2015-01-27 01:26:38 +0000

    ai

--- src/Trail.js
+++ src/Trail.js
@@ -1,16 +1,25 @@
-var Trail = function() {};
+var Trail = function(x, y, death) {
+	this.trail = null;
+	this.x = x;
+	this.y = y;
+	this.death = death;
+};
 
 Trail.prototype = {
 
 	preload: function() {
-		game.load.image('player', 'assets/player.png');
+		game.load.image('trail', 'assets/trail.png');
 	},
 
 	create: function() {
+		this.trail = game.add.sprite(this.x, this.y, 'trail');
+		this.trail.anchor.setTo(.5,.5);
 	},
 
 	update: function() {
-
+		if (death < game.time.totalElapsedSeconds()) {
+			this.trail.kill();
+		}
 	},
 
 	render: function(){
