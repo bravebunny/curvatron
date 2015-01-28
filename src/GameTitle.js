e48d5a1b7918de415f56ba6765fca2e3f9434d6d
Tiago Jerónimo Silva <tiago_j.s@hotmail.com>
2015-01-28 22:07:35 +0000

    menu multiplayer alfa

--- src/GameTitle.js
+++ src/GameTitle.js
@@ -1,8 +1,5 @@
 var gameTitle = function(game){
-	numberOfPlayers = 0;
-	gameMode = 0;
-	gameModes = ["Nomral Mode", "coiso", "Outro coiso"];
-	maxPlayers = 7;
+	this.menuSpace = 160;
 }
 
 gameTitle.prototype = {
@@ -10,100 +7,40 @@ gameTitle.prototype = {
   create: function(){
   		this.game.world.scale.set(1);
 
-		var gameTitle = this.game.add.sprite(0,-320,"gametitle");
+		var gameTitle = this.game.add.sprite(0,-160,"gametitle");
 		gameTitle.anchor.setTo(0.5,0.5);
-		text = this.game.add.text(0,-320, "CurvaTorn", {
+		text = this.game.add.text(0,-160, "CurvaTorn", {
 	        font: "40px Arial",
 	        fill: "#ff0044",
 	        align: "center"
     	});
     	text.anchor.setTo(0.5,0.5);
 
-		var playButton = this.game.add.button(0,-160,"play",this.playTheGame,this);
+		var playButton = this.game.add.button(0,0,"play",this.playTheGame,this);
 		playButton.anchor.setTo(0.5,0.5);
-		text = this.game.add.text(0,-160, "PLAY", {
+		text = this.game.add.text(0,0, "Single Player", {
 	        font: "40px Arial",
 	        fill: "#ff0044",
 	        align: "center"
     	});
     	text.anchor.setTo(0.5,0.5);
 
-    	///////////////////////////////////////Number of players////////////////////////////////////////////////////////
-		var playersAuxButton = this.game.add.sprite(0,0,"auxBar");
-		playersAuxButton.anchor.setTo(0.5,0.5);
-		textPlayers = this.game.add.text(0,0, (numberOfPlayers+1) + " players", {
+    	//Multiplayer
+		var playButton = this.game.add.button(0,160,"play",this.multiplayer,this);
+		playButton.anchor.setTo(0.5,0.5);
+		text = this.game.add.text(0,160, "Multiplayer Player", {
 	        font: "40px Arial",
-	        fill: "#363636",
+	        fill: "#ff0044",
 	        align: "center"
     	});
-    	textPlayers.anchor.setTo(0.5,0.5);
-
-    	var leftArrow = this.game.add.button(0-112,0,"arrow",this.DecNumberOfPlayers,this);
-		leftArrow.anchor.setTo(0.5,0.5);
-
-		var rightArrow = this.game.add.button(0+112,0,"arrow",this.IncNumberOfPlayers,this);
-		rightArrow.anchor.setTo(0.5,0.5);
-
-		///////////////////////////////////////////Game Mode////////////////////////////////////////////////////////////////
-		var playersAuxButton = this.game.add.sprite(0,160,"auxBar");
-		playersAuxButton.anchor.setTo(0.5,0.5);
-		textGameMode = this.game.add.text(0,160, "Normal Game", {
-	        font: "40px Arial",
-	        fill: "#363636",
-	        align: "center"
-    	});
-    	textGameMode.anchor.setTo(0.5,0.5);
-
-    	var leftArrow = this.game.add.button(0-112,160,"arrow",this.DecGameMode,this);
-		leftArrow.anchor.setTo(0.5,0.5);
-
-		var rightArrow = this.game.add.button(0+112,160,"arrow",this.IncGameMode,this);
-		rightArrow.anchor.setTo(0.5,0.5);
-
-		//this.game.state.start("GameMananger");
+    	text.anchor.setTo(0.5,0.5);
 	},
 
 	playTheGame: function(){
-		this.game.state.start("PreloadGame",true,false,numberOfPlayers);
+		this.game.state.start("PreloadGame",true,false,0);
 	},
 
-	DecNumberOfPlayers: function(){
-		if(numberOfPlayers==0){
-			numberOfPlayers=maxPlayers;
-		}
-		else{
-			numberOfPlayers--;
-		}
-		textPlayers.setText("" + (numberOfPlayers+1) + " players");
-	},
-
-	IncNumberOfPlayers: function(){
-		if(numberOfPlayers==maxPlayers){
-		    numberOfPlayers=0;
-	    }
-	    else{
-			numberOfPlayers++;
-	    }
-	    textPlayers.setText("" + (numberOfPlayers+1) + " players");
-	},
-
-	DecGameMode: function(){
-		if(gameMode<1){
-			gameMode = 2;
-		}
-		else{
-			gameMode--;
-		}
-    	textGameMode.setText(gameModes[gameMode]);
-	},
-
-	IncGameMode: function(){
-		if(gameMode>1){
-			gameMode = 0;
-		}
-		else{
-	    	gameMode++;
-		}
-	    textGameMode.setText(gameModes[gameMode]);
+	multiplayer: function(){
+		this.game.state.start("Multiplayer");
 	}
 }
---
+++ src/Multiplayer.js
@@ -0,0 +1,83 @@
+var multiplayer = function(game){
+	maxPlayers = 7;
+	numberOfPlayers = 0;
+};
+  
+multiplayer.prototype = {
+	preload: function(){
+        this.game.load.image("loading","assets/Load.png");
+        this.game.renderer.roundPixels = false;
+
+	    this.game.stage.smoothed = true;
+	},
+  	create: function(){
+
+    	//Number of players
+		var playersAuxButton = this.game.add.sprite(0,0,"auxBar");
+		playersAuxButton.anchor.setTo(0.5,0.5);
+		textPlayers = this.game.add.text(0,0, (numberOfPlayers+1) + " players", {
+	        font: "40px Arial",
+	        fill: "#363636",
+	        align: "center"
+    	});
+    	textPlayers.anchor.setTo(0.5,0.5);
+
+    	var leftArrow = this.game.add.button(0-112,0,"arrow",this.DecNumberOfPlayers,this);
+		leftArrow.anchor.setTo(0.5,0.5);
+
+		var rightArrow = this.game.add.button(0+112,0,"arrow",this.IncNumberOfPlayers,this);
+		rightArrow.anchor.setTo(0.5,0.5);
+
+		//Play Button
+		var playButton = this.game.add.button(0,160,"play",this.playTheGame,this);
+		playButton.anchor.setTo(0.5,0.5);
+		text = this.game.add.text(0,160, "PLAY", {
+	        font: "40px Arial",
+	        fill: "#ff0044",
+	        align: "center"
+    	});
+    	text.anchor.setTo(0.5,0.5);
+
+    	//Go back Button
+		var playButton = this.game.add.button(-480,320,"play",this.back,this);
+		playButton.anchor.setTo(0.5,0.5);
+		text = this.game.add.text(-480,320, "Back", {
+	        font: "40px Arial",
+	        fill: "#ff0044",
+	        align: "center"
+    	});
+    	text.anchor.setTo(0.5,0.5);
+	},
+
+	update: function(){
+		this.game.input.keyboard.addKey(Phaser.Keyboard.ESC).onDown.add(function(){this.game.state.start("GameTitle");}, this);
+	},
+
+	playTheGame: function(){
+		this.game.state.start("PreloadGame",true,false,numberOfPlayers);
+	},
+
+	back:function(){
+		this.game.state.start("GameTitle");
+	},
+
+	DecNumberOfPlayers: function(){
+		if(numberOfPlayers==0){
+			numberOfPlayers=maxPlayers;
+		}
+		else{
+			numberOfPlayers--;
+		}
+		textPlayers.setText("" + (numberOfPlayers+1) + " players");
+	},
+
+	IncNumberOfPlayers: function(){
+		if(numberOfPlayers==maxPlayers){
+		    numberOfPlayers=0;
+	    }
+	    else{
+			numberOfPlayers++;
+	    }
+	    textPlayers.setText("" + (numberOfPlayers+1) + " players");
+	},
+}
