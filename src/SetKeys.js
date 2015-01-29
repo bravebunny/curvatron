3fa09f75b169363f3dd1eef6c92f2fda95079c49
Raicuparta <raicuparta@gmail.com>
2015-01-29 03:20:09 +0000

    add setkeys

---
+++ src/SetKeys.js
@@ -0,0 +1,86 @@
+var setKeys = function(game){
+	this.selectedPlayer = 0;
+	this.keyText = null;
+};
+  
+setKeys.prototype = {
+  create: function(){
+  	//select player
+		var playersAuxButton = this.game.add.sprite(0,0,"auxBar");
+		playersAuxButton.anchor.setTo(0.5,0.5);
+		textPlayers = this.game.add.text(0,0, "Player " + (this.selectedPlayer+1), {
+	        font: "40px Arial",
+	        fill: "#363636",
+	        align: "center"
+  	});
+  	textPlayers.anchor.setTo(0.5,0.5);
+
+    var leftArrow = this.game.add.button(-112,0,"arrow",this.DecSelected,this);
+		leftArrow.anchor.setTo(0.5,0.5);
+
+		var rightArrow = this.game.add.button(+112,0,"arrow",this.IncSelected,this);
+		rightArrow.anchor.setTo(0.5,0.5);
+
+  	//key select button
+		var keyButton = this.game.add.button(0,160,"key",this.selectKey,this);
+		keyButton.anchor.setTo(0.5,0.5);
+		this.keyText = this.game.add.text(0,160, String.fromCharCode(keys[this.selectedPlayer]), {
+	        font: "40px Arial",
+	        fill: "#ff0044",
+	        align: "center"
+  	});
+  	this.keyText.anchor.setTo(0.5,0.5);
+
+  	//back button
+		var backButton = this.game.add.button(-480,320,"play",this.back,this);
+		backButton.anchor.setTo(0.5,0.5);
+		text = this.game.add.text(-480,320, "Back", {
+	        font: "40px Arial",
+	        fill: "#ff0044",
+	        align: "center"
+  	});
+  	text.anchor.setTo(0.5,0.5);
+
+  	this.game.input.keyboard.addCallbacks(this, this.onPressed);
+	},
+
+	update: function(){
+		this.game.input.keyboard.addKey(Phaser.Keyboard.ESC).onDown.add(
+			function(){
+				this.game.state.start("GameTitle");
+			},this);
+
+	},
+
+	back:function(){
+		this.game.state.start("GameTitle");
+	},
+
+	DecSelected: function(){
+		if(this.selectedPlayer==0){
+			this.selectedPlayer=maxPlayers;
+		}
+		else{
+			this.selectedPlayer--;
+		}
+		textPlayers.setText("Player " + (this.selectedPlayer+1));
+		this.keyText.setText(String.fromCharCode(keys[this.selectedPlayer]));
+	},
+
+	IncSelected: function(){
+		if(this.selectedPlayer==maxPlayers){
+		    this.selectedPlayer=0;
+	    }
+	    else{
+				this.selectedPlayer++;
+	    }
+    textPlayers.setText("Player " + (this.selectedPlayer+1));
+    this.keyText.setText(String.fromCharCode(keys[this.selectedPlayer]));
+
+	},
+
+	onPressed: function(){
+		keys[this.selectedPlayer] = this.game.input.keyboard.lastKey.keyCode;
+		this.keyText.setText(String.fromCharCode(keys[this.selectedPlayer]));
+	}
+}
