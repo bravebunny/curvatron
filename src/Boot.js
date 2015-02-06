6f631949a5a9110a69f8b37f0363d69c97963e6f
Raicuparta <raicuparta@gmail.com>
2015-02-06 21:41:54 +0000

    portrait and landscape are my bitches now

--- src/Boot.js
+++ src/Boot.js
@@ -2,8 +2,7 @@ var boot = function(game){};
   
 boot.prototype = {
 	preload: function(){
-		mobile = /Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(navigator.userAgent);
-		//mobile = true;
+
 		bgColors = ['#76b83d', '#cf5e4f', '#805296', '#4c99b9'];
 		bgColorsDark = ['#3b5c1e', '#672f27', '#40294b', '#264c5c'];
 		colorPlayers = ['#eb1c1c','#4368e0','#f07dc1','#44c83a','#9e432e','#3dd6e0','#9339e0','#ebd90f'];
