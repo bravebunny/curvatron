06134f22f4d8073fe8ee743cdc2c0739f0556089
Raicuparta <raicuparta@gmail.com>
2015-02-20 15:51:06 +0000

    Boot cleanup

--- src/Boot.js
+++ src/Boot.js
@@ -1,102 +1,94 @@
 var boot = function(game){
-	orientated: false
+	orientation = "portrait";
+	w2 = 0;
+	h2 = 0;
+	changeColor = false;
+	mute = false;
+
 };
   
 boot.prototype = {
-	init: function () {
-	this.stage.disableVisibilityChange = true;
 
-	this.scale.scaleMode = Phaser.ScaleManager.SHOW_ALL;
-  	this.scale.pageAlignHorizontally = true;
-   	this.scale.pageAlignVertically = true;
-   	this.scale.forceOrientation(true, false);
-   	this.scale.setResizeCallback(this.resize, this);
-   	//this.scale.onSizeChange.add(this.resize, this);
-    //this.scale.refresh();
-
-    this.physics.startSystem(Phaser.Physics.ARCADE);
-
-  	w2 = this.world.width/2;
-	h2 = this.world.height/2;
-
-	auxOrientation = Math.abs(window.orientation) - 90 == 0 ? "landscape" : "portrait";
-	auxWinW = window.innerWidth;
-	auxWinH = window.innerHeight;
-
+	preload: function() {
+	   	this.game.load.image("loading","assets/sprites/menu/loading.png");
 	},
 
-	preload: function(){
-
+  	create: function() {
+		//Background colors
+		//[green, red, purple, blue]
 		bgColors = ['#76b83d', '#cf5e4f', '#805296', '#4c99b9'];
 		bgColorsDark = ['#3b5c1e', '#672f27', '#40294b', '#264c5c'];
+
+		chosenColor = this.game.rnd.integerInRange(0, 3);
+		colorHex = bgColors[chosenColor];
+		colorHexDark = bgColorsDark[chosenColor];
+		document.body.style.background = colorHex;
+		this.stage.backgroundColor = colorHex;
+
+		//Player colors
+		//[red, blue, pink, green, brown, cyan, purple, yellow]
 		colorPlayers = ['#eb1c1c','#4368e0','#f07dc1','#44c83a','#9e432e','#3dd6e0','#9339e0','#ebd90f'];
-	    chosenColor = this.game.rnd.integerInRange(0, 3);
-	    colorHex = bgColors[chosenColor];
-	    colorHexDark = bgColorsDark[chosenColor];
-	    document.body.style.background = colorHex;
-	    this.stage.backgroundColor = colorHex;
-	    changeColor = false;
-	    mute = false;
 
-	   	this.game.load.image("loading","assets/sprites/menu/loading.png");
-	  	this.game.renderer.roundPixels = false;
-	    this.stage.smoothed = true;
+		this.scale.scaleMode = Phaser.ScaleManager.SHOW_ALL;
+		this.scale.pageAlignHorizontally = true;
+	 	this.scale.pageAlignVertically = true;
+	 	this.scale.forceOrientation(true, false);
+	 	this.scale.setResizeCallback(this.resize, this);
 
-	    if (mobile) {
-	    	Cocoon.App.exitCallback(
-	    	function() {
-		    	if (this.state.states[this.game.state.current].backPressed) {
-		    		this.state.states[this.game.state.current].backPressed();
-		    	}
-		    	if (this.state.current == "Menu") {
-		    		return true;
-		    	} else {
-		        return false;
-		    	}
-		    }.bind(this)
-    		);
-  		}
+	  	this.physics.startSystem(Phaser.Physics.ARCADE);
 
-	},
+	  	this.stage.smoothed = false;
 
-  	create: function(){
+		if (mobile) {
+			Cocoon.App.exitCallback(
+				function() {
+					if (this.state.states[this.game.state.current].backPressed) {
+						this.state.states[this.game.state.current].backPressed();
+					}
+					if (this.state.current == "Menu") {
+						return true;
+					} else {
+						return false;
+					}
+				}.bind(this)
+			);
+		}
+
 		this.state.start("PreloadMenu");
 	},
 
 	resize: function() {
-		if((this.state.current != 'GameMananger') && (this.state.current != 'PreloadMenu') && (this.state.current != 'PreloadGame')){
-		    var winW = window.innerWidth;
-		    var winH = window.innerHeight;
-		    var winRatio = winW/winH;
-		    var orientation = Math.abs(window.orientation) - 90 == 0 ? "landscape" : "portrait";
+		if((this.state.current != 'GameMananger') && (this.state.current != 'PreloadMenu') && (this.state.current != 'PreloadGame')) {
+			
+			orientation = Math.abs(window.orientation) - 90 == 0 ? "landscape" : "portrait";
 
-		    var height = Math.round(Math.sqrt(baseArea/winRatio));
-		    var width =  Math.round(winRatio*height);
+			var winW = window.innerWidth;
+			var winH = window.innerHeight;
+			var winRatio = winW/winH;
+			var height = Math.round(Math.sqrt(baseArea/winRatio));
+			var width =  Math.round(winRatio*height);
 
-		    this.game.width = width;
-		    this.game.height = height;
-		    this.stage.width = width;
-		    this.stage.height = height;
-		    this.scale.width = width;
-		    this.scale.height = height;
-			this.game.canvas.width = width;
-			this.game.canvas.height = height;
-			this.game.world.setBounds(0, 0, width, height);
-			this.game.renderer.resize(width, height);
-			this.game.camera.setSize(width, height);
-			this.game.camera.setBoundsToWorld();
+			var game = this.game;
+
+			game.width = width;
+			game.height = height;
+			game.canvas.width = width;
+			game.canvas.height = height;
+			game.renderer.resize(width, height);
+			this.stage.width = width;
+			this.stage.height = height;
+			this.scale.width = width;
+			this.scale.height = height;
+			this.world.setBounds(0, 0, width, height);
+			this.camera.setSize(width, height);
+			this.camera.setBoundsToWorld();
 			this.scale.refresh();
 
 			w2 = this.game.world.width/2;
 			h2 = this.game.world.height/2;
 
 			if (this.state.states[this.game.state.current].setPositions) {
-				if((auxWinW != winW) || (auxWinH != winH) || (auxOrientation != orientation)){
-					auxOrientation = orientation;
-					auxWinW = winW;
-					auxWinH = winH;
-					this.state.states[this.game.state.current].setPositions();
-				}
+				this.state.states[this.game.state.current].setPositions();
 			}
 		}
   	},
