8e275f6e6fab6d07fabcdabb3b1288ab010bb197
Raicuparta <raicuparta@gmail.com>
2015-03-31 14:44:15 +0100

    added Creative mode

---
+++ src/modes/Creative.js
@@ -0,0 +1,30 @@
+var Creative = function (game) {
+	this.sp = true;
+	this.game = game;
+	this.player = null;
+	this.leaderboardID = modesLB[1];
+	this.noCollisions = true;
+};
+
+Creative.prototype = {
+
+	preload: function () {
+		this.game.load.image('player0', 'assets/playerSingle.png');
+		this.game.load.image('trail0', 'assets/trailSingle.png');
+	},
+
+	create: function () {
+		this.player = players[0];
+		scale = 0.3;
+		
+	},
+
+	update: function () {
+
+	},
+
+	erasesTrail: function () {
+		return !this.player.ready;
+	}
+
+};
