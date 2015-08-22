2e734ce3f8fe7404b71414dab7143203ad37cc67
Raicuparta <raicuparta@gmail.com>
2015-08-22 14:48:18 +0100

    menu button prettyfying

--- src/entities/Button.js
+++ src/entities/Button.js
@@ -26,29 +26,38 @@ Button.prototype = {
 		var h = this.h;
 
 		//Button background rectangle
-    this.graphics = this.game.add.graphics(x-w/2, y-h/2);
+    this.graphics = this.game.add.graphics(-w/2, -h/2);
     this.graphics.lineStyle(0);
     this.graphics.beginFill(0xFFFFFF, 1);
     this.graphics.drawRoundedRect(0, 0, w, h, h/2);
     this.graphics.endFill();
 		//this.graphics.anchor.setTo(0.5,0.5);
 
-		//Button icon
-		this.icon = this.game.add.button(x-w/2 + 80, y, this.iconName);
-		this.icon.anchor.setTo(0.5,0.5);
-		this.icon.scale.set(0.7, 0.7);
-		this.icon.tint = parseInt(colorHex.substring(1), 16);
-		//this.icon.hitArea = new Phaser.Circle(0, 0,2*this.radius);
-		this.icon.input.useHandCursor = true;
-		clickButton(this.icon, this.callback, this.context);
-
 		//Button label
-		this.label = this.game.add.text(x+50, y, this.text, {
+		this.label = this.game.add.text(50, 0, this.text, {
 			font: "60px dosis",
-			fill: colorHex,
-			align: "middle"
+			fill: colorHex
 		});
 		this.label.anchor.setTo(0.5,0.5);
+
+		//Button icon
+		this.icon = this.game.add.sprite(-w/2 + 80, 0, this.iconName);
+		this.icon.scale.set(0.7, 0.7);
+		this.icon.tint = parseInt(colorHex.substring(1), 16);
+		//this.icon.hitArea = new Phaser.Rectangle(-80, -80, w, h);
+		this.icon.anchor.setTo(0.5,0.5);
+
+
+		//Button group
+		this.button = this.game.add.button(x, y);
+		//this.icon.hitArea = new Phaser.Rectangle(-80, -80, w, h);
+		this.button.input.useHandCursor = true;
+
+		clickButton(this.button, this.callback, this.context);
+		this.button.addChild(this.graphics);
+		this.button.addChild(this.icon);
+		this.button.addChild(this.label);
+		this.button.anchor.setTo(0.5,0.5);
 	}
 
 };
--- src/states/PreloadMenu.js
+++ src/states/PreloadMenu.js
@@ -9,42 +9,40 @@ preloadMenu.prototype = {
 		loadingBar.body.angularVelocity = 200;
 		this.game.physics.arcade.velocityFromAngle(loadingBar.angle, 300*this.speed, loadingBar.body.velocity);
 		//Load all stuf from menu
-		this.game.load.image("stats_button","assets/sprites/menu/main/stats.png");
-		this.game.load.image("multiplayer_button","assets/sprites/menu/main/multiplayer.png");
-		this.game.load.image("singleplayer_button","assets/sprites/menu/main/singleplayer.png");
-		this.game.load.image("settings_button","assets/sprites/menu/main/settings.png");
+		this.game.load.image("stats_button","assets/sprites/gui/main/stats.png");
+		this.game.load.image("multiplayer_button","assets/sprites/gui/main/multiplayer.png");
+		this.game.load.image("singleplayer_button","assets/sprites/gui/main/singleplayer.png");
+		this.game.load.image("settings_button","assets/sprites/gui/main/settings.png");
 
-		this.game.load.image("endless_button","assets/sprites/menu/singleplayer/endless.png");
-		this.game.load.image("oldSchool_button","assets/sprites/menu/singleplayer/oldSchool.png");
-		this.game.load.image("collecting_button","assets/sprites/menu/singleplayer/collecting.png");
-		this.game.load.image("adventure_button","assets/sprites/menu/singleplayer/adventure.png");
-		this.game.load.image("creative_button","assets/sprites/menu/singleplayer/creative.png");
+		this.game.load.image("endless_button","assets/sprites/gui/singleplayer/endless.png");
+		this.game.load.image("oldSchool_button","assets/sprites/gui/singleplayer/oldSchool.png");
+		this.game.load.image("collecting_button","assets/sprites/gui/singleplayer/collecting.png");
+		this.game.load.image("adventure_button","assets/sprites/gui/singleplayer/adventure.png");
+		this.game.load.image("creative_button","assets/sprites/gui/singleplayer/creative.png");
 
-		this.game.load.image("audio_button","assets/sprites/menu/settings/audio.png");
-		this.game.load.image("audiooff_button","assets/sprites/menu/settings/audiooff.png");
-		this.game.load.image("setkeys_button","assets/sprites/menu/settings/setkeys.png");
-		this.game.load.image("leaderboard_button","assets/sprites/menu/settings/leaderboard.png");
-		this.game.load.image("fullscreen_button","assets/sprites/menu/settings/fullscreen.png");
-		this.game.load.image("player_select","assets/sprites/menu/settings/playerSelect.png");
-		this.game.load.image("key_button","assets/sprites/menu/settings/key.png");
+		this.game.load.image("audio_button","assets/sprites/gui/settings/audio.png");
+		this.game.load.image("audiooff_button","assets/sprites/gui/settings/audiooff.png");
+		this.game.load.image("setkeys_button","assets/sprites/gui/settings/setkeys.png");
+		this.game.load.image("leaderboard_button","assets/sprites/gui/settings/leaderboard.png");
+		this.game.load.image("fullscreen_button","assets/sprites/gui/settings/fullscreen.png");
+		this.game.load.image("player_select","assets/sprites/gui/settings/playerSelect.png");
+		this.game.load.image("key_button","assets/sprites/gui/settings/key.png");
 
+		this.game.load.image("back_button","assets/sprites/gui/navigation/back.png");
+		this.game.load.image("accept_button","assets/sprites/gui/navigation/accept.png");
+		this.game.load.image("exit_button","assets/sprites/gui/navigation/exit.png");
 
+		this.game.load.image("deaths-stats","assets/sprites/gui/stats/deaths-stats.png");
+		this.game.load.image("old-stats","assets/sprites/gui/stats/old-stats.png");
+		this.game.load.image("score-stat","assets/sprites/gui/stats/score-stat.png");
+		this.game.load.image("total-stats","assets/sprites/gui/stats/total-stats.png");
+		this.game.load.image("aux-stat","assets/sprites/gui/stats/aux-stat.png");
+		this.game.load.image("survScore-stat","assets/sprites/gui/stats/endless-stat.png");
 
-		this.game.load.image("back_button","assets/sprites/menu/navigation/back.png");
-		this.game.load.image("accept_button","assets/sprites/menu/navigation/accept.png");
-		this.game.load.image("exit_button","assets/sprites/menu/navigation/exit.png");
-
-		this.game.load.image("deaths-stats","assets/sprites/menu/stats/deaths-stats.png");
-		this.game.load.image("old-stats","assets/sprites/menu/stats/old-stats.png");
-		this.game.load.image("score-stat","assets/sprites/menu/stats/score-stat.png");
-		this.game.load.image("total-stats","assets/sprites/menu/stats/total-stats.png");
-		this.game.load.image("aux-stat","assets/sprites/menu/stats/aux-stat.png");
-		this.game.load.image("survScore-stat","assets/sprites/menu/stats/endless-stat.png");
-
-		this.game.load.image("number_players","assets/sprites/menu/numberPlayers.png");
-		this.game.load.image("number_mod","assets/sprites/menu/numberMod.png");
-		this.game.load.image("set_players","assets/sprites/menu/setPlayers.png");
-		this.game.load.image("resume_button","assets/sprites/menu/resume.png");
+		this.game.load.image("number_players","assets/sprites/gui/numberPlayers.png");
+		this.game.load.image("number_mod","assets/sprites/gui/numberMod.png");
+		this.game.load.image("set_players","assets/sprites/gui/setPlayers.png");
+		this.game.load.image("resume_button","assets/sprites/gui/resume.png");
 
 
 		this.game.load.audio('dream', 'assets/music/dream.ogg');
