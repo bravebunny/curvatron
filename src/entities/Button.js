d7d1df30a63f98c1323db1be23b416367dfa4159
Raicuparta <raicuparta@gmail.com>
2015-09-27 19:18:50 +0100

    standard Creative and Button

--- src/entities/Button.js
+++ src/entities/Button.js
@@ -1,145 +1,136 @@
-//wf and hx are fractions of the width and hight of the screen, respectively
-//if wf = 0.5 and hf = 0.5, the button will stay in the center of the screen.
-//xo and yo are the x and y offsets
+/* global Phaser, colorHex */
 var Button = function (x, y, iconName, text, index, callback, context, game) {
-	this.x = x;
-	this.y = y;
-	this.text = text;
-	this.iconName = iconName;
-	this.callback = callback;
-	this.context = context;
-	this.game = game;
-	this.index = index;
+  this.x = x
+  this.y = y
+  this.text = text
+  this.iconName = iconName
+  this.callback = callback
+  this.context = context
+  this.game = game
+  this.index = index
 
-  this.graphics = null;
-	this.label = null;
-	this.icon = null;
-	this.button = null;
-	this.tween = {};
+  this.graphics = null
+  this.label = null
+  this.icon = null
+  this.button = null
+  this.tween = {}
 
-	this.w = 450;
-	this.h = 100;
-	this.selected = false;
-};
+  this.w = 450
+  this.h = 100
+  this.selected = false
+}
 
 Button.prototype = {
-	create: function () {
-		var x = this.x;
-		var y = this.y;
-		var w = this.w;
-		var h = this.h;
+  create: function () {
+    var x = this.x
+    var y = this.y
+    var w = this.w
+    var h = this.h
 
-		//Button background rectangle
-    this.graphics = this.game.add.graphics(-w/2, -h/2);
-    this.graphics.lineStyle(0);
-    this.graphics.beginFill(0xFFFFFF, 1);
-    this.graphics.drawRoundedRect(0, 0, w, h, h/2);
-    this.graphics.endFill();
-		//this.graphics.anchor.setTo(0.5,0.5);
+    // Button background rectangle
+    this.graphics = this.game.add.graphics(-w / 2, -h / 2)
+    this.graphics.lineStyle(0)
+    this.graphics.beginFill(0xFFFFFF, 1)
+    this.graphics.drawRoundedRect(0, 0, w, h, h / 2)
+    this.graphics.endFill()
+    // this.graphics.anchor.setTo(0.5,0.5)
 
-		var offset = 40;
-		if (this.iconName == null) {
-			offset = 0;
+    var offset = 40
+    if (this.iconName == null) offset = 0
 
-		} else {
+    // Button label
+    this.label = this.game.add.text(offset, 0, this.text, {
+      font: '60px dosis',
+      fill: colorHex
+    })
+    this.label.anchor.setTo(0.5, 0.5)
 
+    // Button icon
+    this.icon = this.game.add.sprite(-w / 2 + 60, 0, this.iconName)
+    this.icon.scale.set(0.5, 0.5)
+    this.icon.tint = parseInt(colorHex.substring(1), 16)
+    // this.icon.hitArea = new Phaser.Rectangle(-80, -80, w, h)
+    this.icon.anchor.setTo(0.5, 0.5)
 
-		}
+    // Button group
+    this.button = this.game.add.button(x, y)
+    // this.icon.hitArea = new Phaser.Rectangle(-80, -80, w, h)
+    this.button.input.useHandCursor = true
 
-		//Button label
-		this.label = this.game.add.text(offset, 0, this.text, {
-			font: "60px dosis",
-			fill: colorHex
-		});
-		this.label.anchor.setTo(0.5,0.5);
+    // clickButton(this.button, this.callback, this.context, menuArray, this.index);
+    this.button.addChild(this.graphics)
+    this.button.addChild(this.icon)
+    this.button.addChild(this.label)
+    this.button.anchor.setTo(0.5, 0.5)
 
-		//Button icon
-		this.icon = this.game.add.sprite(-w/2 + 60, 0, this.iconName);
-		this.icon.scale.set(0.5, 0.5);
-		this.icon.tint = parseInt(colorHex.substring(1), 16);
-		//this.icon.hitArea = new Phaser.Rectangle(-80, -80, w, h);
-		this.icon.anchor.setTo(0.5,0.5);
+    var s = this.button.scale
+    var tweenTime = 30
 
+    this.tween = {
+      press: this.game.add.tween(s).to({x: s.x * 0.9, y: s.y * 0.9}, tweenTime, Phaser.Easing.Linear.None, false),
+      release: this.game.add.tween(s).to({x: s.x, y: s.y}, tweenTime, Phaser.Easing.Linear.None, false),
+      over: this.game.add.tween(s).to({x: s.x * 1.2, y: s.y * 1.2}, tweenTime * 2, Phaser.Easing.Linear.None, false),
+      out: this.game.add.tween(s).to({ x: s.x, y: s.y }, tweenTime * 2, Phaser.Easing.Linear.None, false)
+    }
 
-		//Button group
-		this.button = this.game.add.button(x, y);
-		//this.icon.hitArea = new Phaser.Rectangle(-80, -80, w, h);
-		this.button.input.useHandCursor = true;
+    //tween stuff
+    this.button.onInputOver.add(function () {
+      selection = this.index
+    }, this)
 
-		//clickButton(this.button, this.callback, this.context, menuArray, this.index);
-		this.button.addChild(this.graphics);
-		this.button.addChild(this.icon);
-		this.button.addChild(this.label);
-		this.button.anchor.setTo(0.5,0.5);
+    this.tween.release.onComplete.add(function () {
+      this.selected = false
+      if (!this.tween.release.isRunning && !this.tween.press.isRunning) {
+        this.callback.call(this.context)
+      }
+    }, this)
 
-		var s = this.button.scale;
-	  var tweenTime = 30;
+    this.button.onInputOver.add(function () {
+      this.tween.release.onComplete.active = true
+      // tweenOver.start()
+    }, this)
 
-		this.tween = {
-			press: this.game.add.tween(s).to( { x: s.x*0.9, y: s.y*0.9 }, tweenTime, Phaser.Easing.Linear.None, false),
-			release: this.game.add.tween(s).to( { x: s.x, y: s.y }, tweenTime, Phaser.Easing.Linear.None, false),
-			over: this.game.add.tween(s).to( { x: s.x*1.2, y: s.y*1.2 }, tweenTime*2, Phaser.Easing.Linear.None, false),
-			out: this.game.add.tween(s).to( { x: s.x, y: s.y }, tweenTime*2, Phaser.Easing.Linear.None, false)
-		}
+    this.button.onInputOut.add(function () {
+      this.tween.release.onComplete.active = false
+      // tweenOverOut.start()
+    }, this)
 
-		//tween stuff
-		this.button.onInputOver.add(function() {
-	    selection = this.index;
-	  }, this);
+    this.button.onInputDown.add(this.buttonDown, {
+      tween: this.tween.press,
+      tweenOut: this.tween.release
+    }, this)
 
-		this.tween.release.onComplete.add(function() {
-			this.selected = false;
-			if (!this.tween.release.isRunning && !this.tween.press.isRunning) {
-				this.callback.call(this.context);
-			}
-		}, this);
+    this.button.onInputUp.add(this.buttonUp, {
+      tween: this.tween.release
+    }, this)
+  },
 
-		this.button.onInputOver.add(function() {
-			this.tween.release.onComplete.active = true;
-			//tweenOver.start();
-		}, this);
+  buttonDown: function () {
+    this.tweenOut.onComplete.active = true
+    this.tween.start()
+  },
 
-		this.button.onInputOut.add(function() {
-			this.tween.release.onComplete.active = false;
-			//tweenOverOut.start();
-		}, this);
+  buttonUp: function () {
+    this.tween.start()
+  },
 
-		this.button.onInputDown.add(this.buttonDown, {
-			tween: this.tween.press,
-			tweenOut: this.tween.release
-		}, this);
+  select: function () {
+    this.selected = true
+    selection = this.index
+    this.tween.over.start()
+  },
 
-		this.button.onInputUp.add(this.buttonUp, {
-			tween: this.tween.release
-		}, this);
-	},
+  deselect: function () {
+    this.selected = false
+    this.tween.out.start()
+  },
 
-	buttonDown: function() {
-		this.tweenOut.onComplete.active = true;
-		this.tween.start();
-	},
+  setIcon: function (iconName) {
+    this.icon.loadTexture(iconName)
+  },
 
-	buttonUp: function() {
-		this.tween.start();
-	},
+  setText: function (text) {
+    this.label.text = text
+  }
 
-	select: function() {
-		this.selected = true;
-		selection = this.index;
-		this.tween.over.start();
-	},
-
-	deselect: function() {
-		this.selected = false;
-		this.tween.out.start();
-	},
-
-	setIcon: function(iconName) {
-		this.icon.loadTexture(iconName);
-	},
-
-	setText: function(text) {
-		this.label.text = text;
-	}
-
-};
+}
--- src/modes/Creative.js
+++ src/modes/Creative.js
@@ -1,106 +1,102 @@
+/* global players */
 var Creative = function (game) {
-	this.sp = true;
-	this.game = game;
-	this.player = null;
-	this.leaderboardID = null;
-	this.noCollisions = true;
-	this.popup = null;
-	this.twitter = null;
-	this.rToken = null;
-	this.rTokenSecret = null;
-};
+  this.sp = true
+  this.game = game
+  this.player = null
+  this.leaderboardID = null
+  this.noCollisions = true
+  this.popup = null
+  this.twitter = null
+  this.rToken = null
+  this.rTokenSecret = null
+}
 
 Creative.prototype = {
 
-	preload: function () {
-		this.game.load.image('player0', 'assets/sprites/game/singleplayer/player.png');
-		this.game.load.image('screenshotButton', 'assets/sprites/gui/hud/screenshot.png');
-	},
+  preload: function () {
+    this.game.load.image('player0', 'assets/sprites/game/singleplayer/player.png')
+    this.game.load.image('screenshotButton', 'assets/sprites/gui/hud/screenshot.png')
+  },
 
-	create: function () {
-		this.player = players[0];
-		scale = 0.3;
-		var screenShotSprite = this.game.add.button(100, 100, 'screenshotButton', this.takeScreenShot, this);
-		screenShotSprite.anchor.setTo(0.5, 0.5);
-		screenShotSprite.input.useHandCursor = true;
-		screenShotSprite.scale.set(0.5);
-		screenShotSprite.alpha = 0.2;
+  create: function () {
+    this.player = players[0]
+    scale = 0.3
+    var screenShotSprite = this.game.add.button(100, 100, 'screenshotButton', this.takeScreenShot, this)
+    screenShotSprite.anchor.setTo(0.5, 0.5)
+    screenShotSprite.input.useHandCursor = true
+    screenShotSprite.scale.set(0.5)
+    screenShotSprite.alpha = 0.2
+  },
 
-	},
+  takeScreenShot: function () {
+    /*
+		this.game.canvas.toBlob(function(blob) {
+      //saveAs(blob, "creative.png")
+    });
+		*/
 
-	takeScreenShot: function() {
+    var png = this.game.canvas.toDataURL()
+    this.png = png.replace(/^data:image\/png;base64,/, '')
 
-		/*this.game.canvas.toBlob(function(blob) {
-	    //saveAs(blob, "creative.png");
-		});*/
+    var TwitterAPI = require('node-twitter-api')
+    this.twitter = new TwitterAPI({
+      consumerKey: 'NwssUgdW5A1dKhtzExUFc5AtQ',
+      consumerSecret: 'S4yhhvUDPKAEI4Wqwx9TCqLaQgsdcB8FjhPG6GyMLVK1xzgqeV',
+      callback: 'http://bravebunny.co/'
+    })
 
-		var png = this.game.canvas.toDataURL();
-		this.png = png.replace(/^data:image\/png;base64,/, "");
+    this.twitter.getRequestToken(function (error, requestToken, requestTokenSecret, results) {
+      if (error) {
+        console.log(error)
+      } else {
+        this.rToken = requestToken
+        this.rTokenSecret = requestTokenSecret
+        this.popup = window.open(this.twitter.getAuthUrl(this.rToken))
+      }
+    }.bind(this))
+  },
 
-		var twitterAPI = require('node-twitter-api');
-		this.twitter = new twitterAPI({
-	    consumerKey: 'NwssUgdW5A1dKhtzExUFc5AtQ',
-	    consumerSecret: 'S4yhhvUDPKAEI4Wqwx9TCqLaQgsdcB8FjhPG6GyMLVK1xzgqeV',
-	    callback: 'http://bravebunny.co/'
-		});
+  update: function () {
+    if (this.popup !== null && this.popup.location.href.split('oauth_verifier=')[1] !== undefined) {
+      var oauthVerifier = this.popup.location.href.split('oauth_verifier=')[1]
+      this.popup.close()
+      this.popup = null
 
-		var twitter = this.twitter
+      var aToken, aTokenSecret
 
-		this.twitter.getRequestToken(function(error, requestToken, requestTokenSecret, results){
-	    if (error) {
-	        console.log(error);
-	    } else {
-	      this.rToken = requestToken;
-				this.rTokenSecret = requestTokenSecret;
-				this.popup = window.open(this.twitter.getAuthUrl(this.rToken));
+      this.twitter.getAccessToken(this.rToken, this.rTokenSecret, oauthVerifier, function (error, accessToken, accessTokenSecret, results) {
+        if (error) {
+          console.log(error)
+        } else {
+          aToken = accessToken
+          aTokenSecret = accessTokenSecret
 
-	    }
-		}.bind(this));
-	},
+          var params = {
+            media: this.png,
+            isBase64: true
+          }
 
-	update: function () {
-		if (this.popup != null && this.popup.location.href.split('oauth_verifier=')[1] != undefined) {
-			var oauthVerifier = this.popup.location.href.split('oauth_verifier=')[1];
-			this.popup.close();
-			this.popup = null;
+          this.twitter.uploadMedia(params, aToken, aTokenSecret, function (error, response) {
+            if (error) console.log(error)
+            else {
+              this.twitter.statuses('update', {
+                status: 'I made art with #Curvatron',
+                media_ids: response.media_id_string
+              },
+                aToken,
+                aTokenSecret,
+                function (error, data, response) {
+                  if (error) console.log(error)
+                }
+              )
+            }
+          }.bind(this))
+        }
+      }.bind(this))
+    }
+  },
 
-			var aToken, aTokenSecret;
-
-			this.twitter.getAccessToken(this.rToken, this.rTokenSecret, oauthVerifier, function(error, accessToken, accessTokenSecret, results) {
-				if (error) {
-						console.log(error);
-				} else {
-						aToken = accessToken;
-						aTokenSecret = accessTokenSecret;
-
-						var params = {
-							media: this.png,
-							isBase64: true
-						};
-
-						this.twitter.uploadMedia(params, aToken, aTokenSecret, function(error, response) {
-							if (error) console.log(error);
-							else {
-								this.twitter.statuses("update", {
-							        status: "I made art with #Curvatron",
-											media_ids: response.media_id_string
-							    },
-							    aToken,
-							    aTokenSecret,
-							    function(error, data, response) {
-							        if (error) console.log(error);
-							    }.bind(this)
-								);
-							}
-						}.bind(this))
-				}
-			}.bind(this));
-
-		}
-	},
-
-	erasesTrail: function () {
-		return !this.player.ready;
-	}
-
-};
+  erasesTrail: function () {
+    return !this.player.ready
+  }
+}
