1ce297a30513b8ca5ddec924d05aa3c39a351bca
Tiago Jerónimo Silva <tiago_j.s@hotmail.com>
2015-10-23 17:31:27 +0100

    levelSelector scrollBar v3

--- src/states/LevelSelector.js
+++ src/states/LevelSelector.js
@@ -18,24 +18,28 @@ levelSelector.prototype = {
     this.title.anchor.setTo(0.5, 0.5)
 
     this.containerX = 1.5 * w2
-    this.containerY = h2 * 0.8
+    this.containerY = 300
 
     this.buttons = new ButtonList(this, this.game)
 
     this.buttons.add('back_button', 'back', this.backPressed)
     this.buttons.add('accept_button', 'play', this.play)
+    this.buttons.add('accept_button', 'play', this.play)
+    this.buttons.add('accept_button', 'play', this.play)
+    this.buttons.add('accept_button', 'play', this.play)
+    this.buttons.add('accept_button', 'play', this.play)
+    this.buttons.add('accept_button', 'play', this.play)
 
     this.buttons.create()
-
     this.buttons.select(1)
 
-    this.containerScrollBar = this.game.add.sprite(this.containerX, this.containerY, 'scroll_button')
+    this.containerScrollBar = this.game.add.sprite(this.containerX, this.containerY - 50, 'scroll_button')
     this.containerScrollBar.scale.set(50, 100)
-    this.containerScrollBar.anchor.set(1, 0)
+    this.containerScrollBar.anchor.set(1, 0.5)
 
     this.scrollMask = this.game.add.graphics(0, 0)
     this.scrollMask.beginFill(0xffffff)
-    this.scrollMask.drawRect(w2 * 0.5, h2 * 0.8, 5000, h2 * 1.2)
+    this.scrollMask.drawRect(w2 * 0.5, this.containerY - 100, w2 * 2, h2 * 1.2)
     this.scrollMask.endFill()
 
     this.containerScrollBar.mask = this.scrollMask
@@ -43,13 +47,15 @@ levelSelector.prototype = {
     this.containerScrollBar.input.useHandCursor = true
     this.containerScrollBar.input.enableDrag(false, true)
     this.containerScrollBar.input.allowHorizontalDrag = false
-    this.containerScrollBar.input.boundsRect = new Phaser.Rectangle(this.containerX - this.containerScrollBar.width, this.containerY, 5000, h2 * 1.2)
+    this.containerScrollBar.input.boundsRect = new Phaser.Rectangle(this.containerX - this.containerScrollBar.width, this.containerY - 100, 5000, h2 * 1.2)
+  },
 
-    //this.containerScrollBar.addChild(this.buttons)
+  update: function () {
+    this.buttons.setY(2 * this.containerY - this.containerScrollBar.y - 100)
+    this.buttons.update()
   },
 
   play: function () {
-    numberPlayers = 0
     var mode = new Adventure(this.game, false)
     this.game.state.start('PreloadGame', true, false, mode, 'assets/levels/level1')
   },
