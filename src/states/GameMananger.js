190525023dc0415d37bdc6102a7526e9c3db50bc
Raicuparta <raicuparta@gmail.com>
2015-11-29 23:43:39 +0000

    hidden cursor setting icon

--- src/states/GameMananger.js
+++ src/states/GameMananger.js
@@ -5,7 +5,7 @@ muteAudio:true, paused:true, totalTime:true, pauseTween:true, borders:true,
 colisionMargin:true, nextBallHigh:true, changeColor:true, killSound:true,
 collectSound:true, Phaser, w2, h2, groupPowers:true, tempLabelText:true,
 colorHex, Player, keys, colorHexDark, bgColor:true, muteMusic:true, muteSoundEffects:true, ButtonList,
-clickButton, localStorage, saveAs, countdown:true, Screenshot, Rotator, Horizontal, Vertical, showMouse */
+clickButton, localStorage, saveAs, countdown:true, Screenshot, showMouse */
 /*eslint-enable*/
 var gameMananger = function (game) {
   tempLabel = null
