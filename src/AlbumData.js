fac9e31da0de18944cb13a8ab8789de6050f3416
Raicuparta <raicuparta@gmail.com>
2015-12-01 09:57:29 +0000

    changed AlbumData style

--- src/AlbumData.js
+++ src/AlbumData.js
@@ -1,204 +1,31 @@
-var musicList = []
-
-musicList.push({
-  file: 'lpchip',
-  title: 'LPchip',
-  author: 'LPchip',
-  site: 'lpchip.nl'
-})
-
-musicList.push({
-  file: 'no_memory',
-  title: 'No Memory',
-  author: 'ShiggyBeatz',
-  site: 'shiggybeatz.com'
-})
-
-musicList.push({
-  file: 'decembrr',
-  title: 'Decembrr',
-  author: 'VIAES',
-  site: 'soundcloud.com/viaes'
-})
-
-musicList.push({
-  file: 'esplanade',
-  title: 'Esplanade',
-  author: 'James Magnum',
-  site: 'soundcloud.com/jamesmagnum'
-})
-
-musicList.push({
-  file: 'good_news',
-  title: 'Good News',
-  author: 'Inside People',
-  site: 'insidepeoplemusic.com'
-})
-
-musicList.push({
-  file: 'courting_for_puss',
-  title: 'Courting for Puss',
-  author: 'Adeus Senhor F.',
-  site: 'soundcloud.com/adeussenhorf'
-})
-
-musicList.push({
-  file: 'geometry',
-  title: 'Geometry',
-  author: 'Tim Gerwing',
-  site: 'lascaux21.com'
-})
-
-musicList.push({
-  file: 'tone_town',
-  title: 'Tone Town',
-  author: 'Ishan S',
-  site: 'youtube.com/channel/UCxpjs6EPRocamfPmAQWw4iQ'
-})
-
-musicList.push({
-  file: 'maple',
-  title: 'Maple',
-  author: 'The Amplified Apple',
-  site: 'soundcloud.com/theamplifiedapple'
-})
-
-musicList.push({
-  file: 'looking_for_pluto',
-  title: 'Looking For Pluto',
-  author: 'Benedikt',
-  site: 'soundcloud.com/bendikhjaltason'
-})
-
-musicList.push({
-  file: 'afterglow',
-  title: 'Afterglow',
-  author: 'Maximus Quackhandle',
-  site: 'soundcloud.com/maximusquackhandle'
-})
-
-musicList.push({
-  file: 'fever_dream',
-  title: 'Fever Dream',
-  author: 'Eli G and WPH',
-  site: 'youtube.com/channel/UCB4FEwdbXwnwRPvBXY8BO7A'
-})
-
-musicList.push({
-  file: 'whereknow',
-  title: 'whereknow',
-  author: 'Rick Sparks',
-  site: 'ricksparks.bandcamp.com'
-})
-
-musicList.push({
-  file: 'synthetic_popz',
-  title: 'Synthetic Popz',
-  author: 'O.J.B.',
-  site: 'soundcloud.com/ljohnson91-1'
-})
-
-musicList.push({
-  file: 'preamble',
-  title: 'Preamble (L)',
-  author: 'Walrus Tales',
-  site: 'walrustales.com'
-})
-
-musicList.push({
-  file: 'suburban_lunacy',
-  title: 'Suburban Lunacy',
-  author: 'Ray AB',
-  site: 'rayab.bandcamp.com'
-})
-
-musicList.push({
-  file: 'delta',
-  title: 'Delta',
-  author: 'Face Of Bear',
-  site: 'soundcloud.com/face-of-bear'
-})
-
-musicList.push({
-  file: 'pleasantville',
-  title: 'Pleasantville',
-  author: 'Hybrid Junkie',
-  site: 'soundcloud.com/daftmau569'
-})
-
-musicList.push({
-  file: 'julias_song',
-  title: 'Julia\'s Song',
-  author: 'K-o-m-o-d-o',
-  site: 'k-o-m-o-d-o.bandcamp.com'
-})
-
-musicList.push({
-  file: 'pay_careful_attention',
-  title: 'Pay Careful Attention',
-  author: 'The Tin Pans',
-  site: 'thetinpans.bandcamp.com'
-})
-
-musicList.push({
-  file: 'black_dog',
-  title: 'Black Dog',
-  author: 'Hanray',
-  site: 'soundcloud.com/hanray-1'
-})
-
-musicList.push({
-  file: 'je_taime_tu_me_manques',
-  title: 'je t\'aime, tu me manques',
-  author: 'unnq',
-  site: 'soundcloud.com/unnq'
-})
-
-musicList.push({
-  file: 'aug14_2008',
-  title: 'Aug14-2008',
-  author: 'Steven Danglis',
-  site: 'soundcloud.com/steven-danglis'
-})
-
-musicList.push({
-  file: 'yosemite',
-  title: 'Yosemite',
-  author: 'James S. Reid',
-  site: 'thelabrats.net'
-})
-
-musicList.push({
-  file: 'the_overview_of_space',
-  title: 'the overview of space',
-  author: 'tobi',
-  site: 'tobiwan.bandcamp.com'
-})
-
-musicList.push({
-  file: 'patter',
-  title: 'Patter',
-  author: 'Heat Legend',
-  site: 'soundcloud.com/heatlegendmusic'
-})
-
-musicList.push({
-  file: ' incandescence',
-  title: 'Incandescence',
-  author: 'Lachy',
-  site: 'soundcloud.com/lstevprods'
-})
-
-musicList.push({
-  file: 'yna',
-  title: 'Yna',
-  author: 'Ry JT',
-  site: 'soundcloud.com/reble-t'
-})
-
-musicList.push({
-  file: 'mellow',
-  title: 'Mellow',
-  author: 'Kasse',
-  site: 'soundcloud.com/spacholi'
-})
+var musicList = [
+  {file: 'lpchip', title: 'LPchip', author: 'LPchip', site: 'lpchip.nl'},
+  {file: 'no_memory', title: 'No Memory', author: 'ShiggyBeatz', site: 'shiggybeatz.com'},
+  {file: 'decembrr', title: 'Decembrr', author: 'VIAES', site: 'soundcloud.com/viaes'},
+  {file: 'esplanade', title: 'Esplanade', author: 'James Magnum', site: 'soundcloud.com/jamesmagnum'},
+  {file: 'good_news', title: 'Good News', author: 'Inside People', site: 'insidepeoplemusic.com'},
+  {file: 'courting_for_puss', title: 'Courting for Puss', author: 'Adeus Senhor F.', site: 'soundcloud.com/adeussenhorf'},
+  {file: 'geometry', title: 'Geometry', author: 'Tim Gerwing', site: 'lascaux21.com'},
+  {file: 'tone_town', title: 'Tone Town', author: 'Ishan S', site: 'youtube.com/channel/UCxpjs6EPRocamfPmAQWw4iQ'},
+  {file: 'maple', title: 'Maple', author: 'The Amplified Apple', site: 'soundcloud.com/theamplifiedapple'},
+  {file: 'looking_for_pluto', title: 'Looking For Pluto', author: 'Benedikt', site: 'soundcloud.com/bendikhjaltason'},
+  {file: 'afterglow', title: 'Afterglow', author: 'Maximus Quackhandle', site: 'soundcloud.com/maximusquackhandle'},
+  {file: 'fever_dream', title: 'Fever Dream', author: 'Eli G and WPH', site: 'youtube.com/channel/UCB4FEwdbXwnwRPvBXY8BO7A'},
+  {file: 'whereknow', title: 'whereknow', author: 'Rick Sparks', site: 'ricksparks.bandcamp.com'},
+  {file: 'synthetic_popz', title: 'Synthetic Popz', author: 'O.J.B.', site: 'soundcloud.com/ljohnson91-1'},
+  {file: 'preamble', title: 'Preamble (L)', author: 'Walrus Tales', site: 'walrustales.com'},
+  {file: 'suburban_lunacy', title: 'Suburban Lunacy', author: 'Ray AB', site: 'rayab.bandcamp.com'},
+  {file: 'delta', title: 'Delta', author: 'Face Of Bear', site: 'soundcloud.com/face-of-bear'},
+  {file: 'pleasantville', title: 'Pleasantville', author: 'Hybrid Junkie', site: 'soundcloud.com/daftmau569'},
+  {file: 'julias_song', title: 'Julia\'s Song', author: 'K-o-m-o-d-o', site: 'k-o-m-o-d-o.bandcamp.com'},
+  {file: 'pay_careful_attention', title: 'Pay Careful Attention', author: 'The Tin Pans', site: 'thetinpans.bandcamp.com'},
+  {file: 'black_dog', title: 'Black Dog', author: 'Hanray', site: 'soundcloud.com/hanray-1'},
+  {file: 'je_taime_tu_me_manques', title: 'je t\'aime, tu me manques', author: 'unnq', site: 'soundcloud.com/unnq'},
+  {file: 'aug14_2008', title: 'Aug14-2008', author: 'Steven Danglis', site: 'soundcloud.com/steven-danglis'},
+  {file: 'yosemite', title: 'Yosemite', author: 'James S. Reid', site: 'thelabrats.net'},
+  {file: 'the_overview_of_space', title: 'the overview of space', author: 'tobi', site: 'tobiwan.bandcamp.com'},
+  {file: 'patter', title: 'Patter', author: 'Heat Legend', site: 'soundcloud.com/heatlegendmusic'},
+  {file: ' incandescence', title: 'Incandescence', author: 'Lachy', site: 'soundcloud.com/lstevprods'},
+  {file: 'yna', title: 'Yna', author: 'Ry JT', site: 'soundcloud.com/reble-t'},
+  {file: 'mellow', title: 'Mellow', author: 'Kasse', site: 'soundcloud.com/spacholi'}
+]
