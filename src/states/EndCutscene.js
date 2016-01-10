95952f23adcf4680c1e287df0372a9d6b11d9a21
Raicuparta <raicuparta@gmail.com>
2016-01-10 15:49:04 +0000

    end cutscene

--- src/states/EndCutscene.js
+++ src/states/EndCutscene.js
@@ -2,6 +2,8 @@ var endCutscene = function (game) {
   this.game = game
   this.pics = []
   this.text = []
+  this.delay = 1000
+  this.duration = 3000
 }
 
 endCutscene.prototype = {
@@ -22,7 +24,7 @@ endCutscene.prototype = {
     this.pics[2].alpha = 0
 
     var tween = this.game.add.tween(this.pics[0])
-    tween.to({alpha:1}, 2000, Phaser.Easing.Linear.None).delay(500)
+    tween.to({alpha:1}, this.duration, Phaser.Easing.Linear.None).delay(this.delay)
     tween.onComplete.add(function () {
       this.addText(100, 100, this.next1,
         'After collecting all the point pellets, the snake moved to New Zealand.')
@@ -38,17 +40,19 @@ endCutscene.prototype = {
       fill: '#ffffff',
       align: 'left',
     })
-    text.setShadow(-3, 3, 'rgba(0,0,0,0.8)', 5)
+    text.setShadow(-3, 3, 'rgba(0,0,0,1)', 10)
+    text.stroke = '#000000';
+    text.strokeThickness = 1;
     text.fixedToCamera = true
     text.alpha = 0
 
     var move = this.game.add.tween(text.cameraOffset)
     console.log(text.y)
-    move.to({y: y}, 2000, Phaser.Easing.Quadratic.Out).delay(500)
+    move.to({y: y}, this.duration, Phaser.Easing.Quadratic.Out).delay(this.delay)
     move.start()
 
     var fadeIn = this.game.add.tween(text)
-    fadeIn.to({alpha:1}, 2000, Phaser.Easing.Linear.None).delay(500)
+    fadeIn.to({alpha:1}, this.duration, Phaser.Easing.Linear.None).delay(this.delay)
     fadeIn.onComplete.add(next, this)
     fadeIn.start()
 
@@ -60,6 +64,62 @@ endCutscene.prototype = {
   },
 
   next2: function () {
+    this.addText(100, 300, this.next3,
+      'But life doesn\'t always work out the way we want it to...')
+  },
+
+  next3: function () {
+    this.pics[1].bringToTop()
+    var tween = this.game.add.tween(this.pics[1])
+    tween.to({alpha:1}, this.duration, Phaser.Easing.Linear.None).delay(this.delay*3)
+    tween.onComplete.add(function () {
+      this.addText(100, 100, this.next4,
+        'The snake met the love of its life, Bob, and they got married.')
+    }, this)
+    tween.start()
+  },
+
+  next4: function () {
+    this.addText(100, 200, this.next5,
+      'They had a daughter, and the snake had to put the law degree on hold to care for her.')
+  },
+
+  next5: function () {
+    this.addText(100, 300, this.next6,
+      'But one day Bob left with a curvier snake, and never came back.')
+  },
+
+  next6: function () {
+    this.pics[2].bringToTop()
+    var tween = this.game.add.tween(this.pics[2])
+    tween.to({alpha:1}, this.duration, Phaser.Easing.Linear.None).delay(this.delay*3)
+    tween.onComplete.add(function () {
+      this.addText(100, 100, this.next7,
+        'Now, the snake supports her child by working as a cashier at a local grocery store.')
+    }, this)
+    tween.start()
+  },
+
+  next7: function () {
+    this.addText(100, 200, this.next8,
+      'Some times, she still thinks about her old days as a point pellet collector...')
+  },
+
+  next8: function () {
+   var overlay = this.game.add.sprite(0, 0, 'overlay')
+    overlay.width = this.pics[0].width
+    overlay.height = this.pics[0].height
+    overlay.alpha = 0
+    var fadeOut = this.game.add.tween(overlay)
+    fadeOut.to({alpha:1}, this.duration, Phaser.Easing.Linear.None).delay(this.delay*3)
+    fadeOut.onComplete.add(function () {
+      this.addText(100, 100, this.next9,
+        'You have unlocked the Hard Mode levels.')
+    }, this)
+    fadeOut.start()
+  },
+
+  next9: function () {
 
   },
 
